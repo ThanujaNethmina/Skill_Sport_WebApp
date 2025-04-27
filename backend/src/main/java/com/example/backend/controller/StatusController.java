4c183b0130771c2aa6c67d6fa5bfe92b1fe0899e
Dinethra Sulakshana <151991473+Dinethra2001@users.noreply.github.com>
2025-04-27 07:04:03 +0530

    create StatusController.java file

---
+++ backend/src/main/java/com/example/backend/controller/StatusController.java
@@ -0,0 +1,22 @@
+package com.example.backend.controller;
+
+import com.example.backend.model.Status;
+import com.example.backend.service.StatusService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/story")
+public class StatusController {
+    
+}
